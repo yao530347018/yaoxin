aa32ae9c324258910d8618a5a4a673e2192502fe
yao <530347018@qq.com>
2019-07-05 08:20:47 +0800

    第一次使用idea提交

---
+++ git_test/src/main/java/com/itheima/demo.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+/**
+ * @author ：yx
+ * @date ：Created in 2019/7/5 0005
+ * @description ：
+ * @version: 1.0
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("我是张三");
+    }
+}
